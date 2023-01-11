724a0c92d73008523783fc1be7eba2bab95b22aa
Rafael Vargas <Rafael Vargas>
2023-01-11 14:30:12 -0300

    Introdução

---
+++ intro/index.js
@@ -0,0 +1,5 @@
+"use strict";
+exports.__esModule = true;
+var msg = "string 1 aaaa";
+console.log(msg);
+exports["default"] = msg;
---
+++ intro/index.ts
@@ -0,0 +1,5 @@
+const msg = "string 1 aaaa";
+
+console.log(msg);
+
+export default msg;
