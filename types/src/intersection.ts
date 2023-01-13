8f90b695622b44d247462d1220a84b39ee055a6f
Rafael Vargas <rafaelrgsv@gmail.com>
2023-01-13 13:21:42 -0300

    Intersection

---
+++ types/src/intersection.ts
@@ -0,0 +1,21 @@
+type User = {
+    name: string,
+    id: number
+}
+
+type LevelAdmin = "teacher" | "coordenator" | "manager";
+
+type Admin = {
+    isAdmin: true,
+    level: LevelAdmin
+}
+
+//Intersection é representado pelo símbolo '&' e pode ser interpretado como 'e' e significa a união de duas coisas. No caso abaixo, a união de dois objetos e uma instância desse objeto deve ter todas as propriedade dos dois objetos
+type UserAdmin  = User & Admin
+
+let Rafael: UserAdmin = {
+    name: "Rafael",
+    id: 0, 
+    isAdmin: true,
+    level: "coordenator"
+}
